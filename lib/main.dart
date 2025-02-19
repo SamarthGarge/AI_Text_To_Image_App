ce1d4cdb48b709cc9f264c7aadaf33003c909d44
Samarth Garge <gargesamarth@gmail.com>
2025-02-19 11:32:36 +0530

    init

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:ai_text_to_image_app/image_generator.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: "assets/.env"); // Load the .env from assets
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: AiTextToImageGenerator(),
+    );
+  }
+}
